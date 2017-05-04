9330b2021cb1ecd4083e5c0e4c5f574fd5b6f37c
mishellscripts <michelle.song@sjsu.edu>
2017-05-03 17:34:32 -0700

    Updates

--- UnixIO.c
+++ UnixIO.c
@@ -31,12 +31,6 @@ int main()
 	// Child 5: Prompts in terminal, write string to parent thru pipe
 	// Parent uses select() to choose which message to write to output.txt
 
-	//time_t curTime;
-	//gettimeofday(&tv, NULL);
-	//printf(tv.tv_sec);
-	//curTime = tv.tv_sec;
-	//printf("%d\n", curTime);
-
 	gettimeofday(&tv1, NULL);
 	time_t startTime = time(NULL);
 
@@ -53,28 +47,25 @@ int main()
 	int i;
 	int n = 5;
 
+
+
 	/* Start children. */
 	for (i = 0; i < n; ++i) {
 		if ((pids[i] = fork()) < 0) {
 			perror("fork");
 			abort();
-		} else if (pids[i] == 0) {
+		}
+		else if (pids[i] == 0) {
 			//if (pipe(fds[i][0] == -1)) {
 			//	fprintf(stderr,"pipe() failed");
 			//	return 1;
 			//}
 			pipe(fds[i]);
+
 			//Work for child process
 
-
-			//for(int x = 0; x < 5; x++) {
 			while(time(NULL) - startTime < 5) {
 				int sleepDuration = rand() % 3;
-				//alarm(2);
-
-				//time_t currentTime = time(NULL);
-				//time_t elapsedTime = currentTime - startTime;
-				//printf("%d\n", elapsedTime);
 
 				gettimeofday(&tv2, NULL);
 
@@ -88,58 +79,25 @@ int main()
 				time_t remainingmsec = milsec % 1000;
 
 				char *timestamp = malloc(100);
-				sprintf(timestamp, "%d:%.2d.%.3d: Child %d message %d\n", min,
+				sprintf(timestamp, "%d:%.2d.%.3d: Child %d message %d", min,
 						remainingsec, remainingmsec, i, messageNum);
-				//min + ":" + remainingsec + "." + remainingmsec + "\n";
 
 				write(fds[i][WRITE_END], timestamp, strlen(timestamp)+1);
-				//printf("%d:%.2d.%.3d\n", min, remainingsec, remainingmsec);
-				puts(timestamp);
-
-				// Close the unused WRITE end of the pipe.
-				close(fds[i][WRITE_END]);
-
-				// Read from the READ end of the pipe.
-				//read(fd[READ_END], read_msg, BUFFER_SIZE);
-				//printf("Child %d: Read '%s' from the pipe.\n", i, read_msg);
-				// Close the READ end of the pipe.
-				close(fds[i][READ_END]);
+				printf("Child writes %s\n", timestamp);
 
 				printf("Child %d sleeps for %d sec\n\n", i, sleepDuration);
 				sleep(sleepDuration);
 				messageNum++;
+
+
+				//close(fds[i][WRITE_END]);
+				read(fds[i][READ_END], read_msg, BUFFER_SIZE);
+				printf("Parent: Read '%s' from the pipe.\n", read_msg);
 			}
-			//}
+			close(fds[i][READ_END]);
 		}
-
-
-
-
-
-
-
-		exit(0);
+		//exit(0);
 	}
-
-	//puts("done");
-
-	/* Wait for children to exit. */
-	int status;
-	pid_t pid = 1;
-
-
-
-
-
-	while (n > 0) {
-		pid = wait(&status);
-		//printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
-
-		printf("done\n");
-
-		--n;  // TODO(pts): Remove pid from the pids array.
-	}
-
 	return 0;
 }
 
