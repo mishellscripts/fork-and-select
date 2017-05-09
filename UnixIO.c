43415b7ed379c46b58d86ca3150eb5b3adb2c1ec
mishellscripts <michelle.song@sjsu.edu>
2017-05-09 02:23:07 -0700

    Child 5 functionality added

--- UnixIO.c
+++ UnixIO.c
@@ -61,17 +61,42 @@ int main()
 	/* Start children. */
 	for (i = 0; i < n; ++i) {
 
-
-		char message[300];
+		char message[128];
 		if (i == 4) {
-			while(time(NULL) - startTime < 5) {
-				printf("Enter message: \n");
+			while(time(NULL) - startTime < 10) {
+				FD_ZERO(&read_set);
+				FD_SET(fds[i][READ_END], &read_set);
+				gettimeofday(&tv2, NULL);
+
+				unsigned long microsec = (1000000 * tv2.tv_sec + tv2.tv_usec)-(1000000 * tv1.tv_sec + tv1.tv_usec);
+
+				time_t milsec = microsec/1000;
+				time_t sec = milsec/1000;
+				time_t min = sec/60;
+				time_t remainingsec = sec % 60;
+				time_t remainingmsec = milsec % 1000;
+
+
+				printf("\nEnter message: \n");
 				scanf("%s", &message);
 				puts("");
+
+				char *timestamp = malloc(256);
+
+				sprintf(timestamp, "%d:%.2d.%.3d: Child %d message %s", min,
+						remainingsec, remainingmsec, i+1, message);
+
+				write(fds[i][WRITE_END], timestamp, strlen(timestamp)+1);
+				fprintf(fPtr, "Child %d writes %s\n", i+1, timestamp);
+
+
+				rc = select(FD_SETSIZE, &read_set, NULL, NULL, &timeout);
+				if (rc > 0) {
+					read(fds[4][READ_END], read_msg, BUFFER_SIZE);
+					fprintf(fPtr, "Parent: Read '%s' from the pipe.\n", read_msg);
+				}
 			}
 		}
-
-
 		sleepDuration = rand() % 3;
 		if (pipe(fds[i]) == -1) {
 			perror("pipe");
@@ -83,9 +108,8 @@ int main()
 		}
 		else if (pids[i] == 0) {
 			//Work for child process
-			while(time(NULL) - startTime < 5) {
-
-				FD_ZERO(&read_set);
+			while(time(NULL) - startTime < 30) {
+				//FD_ZERO(&read_set);
 				FD_SET(fds[i][READ_END], &read_set);
 				sleepDuration = rand() % 3;
 				gettimeofday(&tv2, NULL);
@@ -100,86 +124,30 @@ int main()
 
 				char *timestamp = malloc(500);
 
-
-
-				if(i == 4) {
-
-					/*char message[300];
-					int nread;
-					if(FD_ISSET(fds[i][READ_END], &read_set)) {
-						//puts("duitty");
-						//printf(">>>>>");
-
-						//scanf("%c\n", &message);
-						ioctl(fds[i][READ_END], FIONREAD, &nread);
-						puts("");
-						if (nread == 0) {
-							//puts("sad");
-							//exit(0);
-						}
-						else {
-						nread = read(fds[i][READ_END], message, nread);
-						message[nread] = 0;
-						printf("Read %d characters from the keyboard: %s\n", nread, message);
-						}
-					}
-					printf("Enter message: \n");
-
-					scanf("%s", &message);
-					puts("");*/
-					//fprintf(stdout, "[child] Enter message: ");
-					//fgets(message, 80, stdin);
-					//printf("[child] line = %s\n", message);
-					sprintf(timestamp, "5th >>>> %d:%.2d.%.3d: Child %d message %s", min,
-							remainingsec, remainingmsec, i+1, message);
-				}
-				else { //for remaining 4 children
+				if(i != 4) {
 					sprintf(timestamp, "%d:%.2d.%.3d: Child %d message %d", min,
 							remainingsec, remainingmsec, i+1, messageNum);
 					messageNum++;
-				}
 
 
+					write(fds[i][WRITE_END], timestamp, strlen(timestamp)+1);
+					fprintf(fPtr, "Child %d writes %s\n", i+1, timestamp);
 
-				write(fds[i][WRITE_END], timestamp, strlen(timestamp)+1);
-				//printf("Child %d writes %s\n", i, timestamp);
-				fprintf(fPtr, "Child %d writes %s\n", i+1, timestamp);
-
-				if(i != 4) {
-					//printf("Child %d sleeps for %d sec\n\n", i, sleepDuration);
 					fprintf(fPtr, "Child %d sleeps for %d sec\n\n", i+1, sleepDuration);
 					sleep(sleepDuration);
+
+					// Select
+					rc = select(FD_SETSIZE, &read_set, NULL, NULL, &timeout);
+					if (rc > 0) {
+						read(fds[i][READ_END], read_msg, BUFFER_SIZE);
+						fprintf(fPtr, "Parent: Read '%s' from the pipe.\n", read_msg);
+					}
 				}
-
-				//select
-				//FD_SET(fds[i][READ_END], &read_set);
-
-				rc = select(FD_SETSIZE, &read_set, NULL, NULL, &timeout);
-				//if (FD_ISSET(fds[i][READ_END], &read_set)) {
-				if (rc > 0) {
-					//close(fds[i][WRITE_END]);
-					read(fds[i][READ_END], read_msg, BUFFER_SIZE);
-					//read(&read_set, read_msg, BUFFER_SIZE);
-					//printf("Parent: Read '%s' from the pipe.\n", read_msg);
-					fprintf(fPtr, "Parent: Read '%s' from the pipe.\n", read_msg);
-					//close(READ_END);
-				}
-				else {
-					//puts("None read");
-				}
-
-
 
 			}
 			break;
-			//exit(0);
-			// FD_ZERO(&fds);    	// initialize inputs
-			//FD_SET(0, &inputs);  	// set file descriptor 0 (stdin)
 		}
 	}
-	//puts("done");
-
 	fclose(fPtr);
 	return 0;
 }
-
